90a886694f9fc5987a1e85fb614156ba029d2f6f
Someshwari <someshwariwarwatte@gmail.com>
2025-09-15 21:03:26 +0530

    Create LongestRepeatingSubsequence.java

---
+++ LongestRepeatingSubsequence.java
@@ -0,0 +1,24 @@
+public class LongestRepeatingSubsequence {
+    public static void main(String[] args) {
+        String str = "axxxy";
+        int n = str.length();
+        System.out.println("Length of Longest Repeating Subsequence: " + lrs(str, n));
+    }
+
+    public static int lrs(String str, int n) {
+        int[][] dp = new int[n + 1][n + 1];
+
+        // Build DP table similar to LCS
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str.charAt(i - 1) == str.charAt(j - 1) && i != j) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][n];
+    }
+}
