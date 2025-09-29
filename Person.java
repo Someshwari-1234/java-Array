63d06f9a14c5b33ffcd13bf7b853b30d6f9caafd
Someshwari <someshwariwarwatte@gmail.com>
2025-09-29 22:35:43 +0530

    Update Person.java
    This program uses equals() when you want to compare objects by value/content and uses hashCode() when storing objects in hash-based collections.

--- Person.java
+++ Person.java
@@ -1,8 +1,10 @@
-class Person {
+import java.util.Objects;
+
+class Test {
     String name;
     int id;
 
-    Person(String name, int id) {
+    Test(String name, int id) {
         this.name = name;
         this.id = id;
     }
@@ -10,8 +12,28 @@ class Person {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Person)) return false;
-        Person p = (Person) o;
-        return id == p.id && name.equals(p.name);
+        if (!(o instanceof Test)) return false;
+        Test t = (Test) o;
+        return id == t.id && name.equals(t.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id); // generates a good hash
+    }
+}
+
+public class Person {
+    public static void main(String[] args) {
+        Test p1 = new Test("Alice", 1);
+        Test p2 = new Test("Alice", 1);
+
+        System.out.println(p1.equals(p2)); // true
+        System.out.println(p1.hashCode() == p2.hashCode()); // true
+
+        HashSet<Test> set = new HashSet<>();
+        set.add(p1);
+        set.add(p2);
+        System.out.println(set.size()); //  correct
     }
 }
