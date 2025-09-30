842c1e2337639707553f233d4dffb76cef741ccb
Someshwari <someshwariwarwatte@gmail.com>
2025-09-30 23:37:17 +0530

    Create CountArray.java
    Count the elements of an Array.

---
+++ CountArray.java
@@ -0,0 +1,8 @@
+package arraysexamples;
+
+public class CountArray {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5, 6, 7};
+        System.out.println("Total number of elements = " + arr.length);
+    }
+}
